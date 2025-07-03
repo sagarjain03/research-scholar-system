6f5090636824d06fc707871d50b9ad1cf1473762
sagarjain03 <141217003+sagarjain03@users.noreply.github.com>
2025-07-03 22:14:39 +0530

    login and profile api endpoints created

---
+++ app/api/users/profile/route.ts
@@ -0,0 +1,30 @@
+import { connectDB } from "@/db/dbconfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+
+connectDB();
+
+export async function GET(request: NextRequest) {
+  try {
+    const userId = getDataFromToken(request);
+
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const user = await User.findById(userId).select("-password");
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({
+      message: "Profile fetched successfully",
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.error("Profile error:", error);
+    return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
+  }
+}
